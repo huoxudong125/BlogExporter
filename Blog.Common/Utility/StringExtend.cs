760094092fbce85d1321695c6a50d06754939ea8
huoxudong125 <423huo@163.com>
2015-09-05 16:22:00 +0800

    Fix Web Exporter using Encoding.UTF8

--- Blog.Common/Utility/StringExtend.cs
+++ Blog.Common/Utility/StringExtend.cs
@@ -4,11 +4,21 @@ namespace Blog.Common
 {
     public static class StringExtend
     {
+        /// <summary>
+        /// Clear tab char & Clear NewLine
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static string ClearNotWords(this string str)
         {
             return str.Replace(@"\t", String.Empty).Replace(@"\n", String.Empty).Trim();
         }
 
+        /// <summary>
+        /// Make the FileName is validate name, By removing invalid chars.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public static string ToValidFileName(this string fileName)
         {
             foreach (char c in System.IO.Path.GetInvalidFileNameChars())
--- Blog.Process/WebUtilityExpoter.cs
+++ Blog.Process/WebUtilityExpoter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Blog.Common;
 using Blog.Process.Interfaces;
@@ -9,6 +11,25 @@ namespace Blog.Process
 {
     public class WebUtilityExpoter : IExporter
     {
+        string htmlString = @"<!doctype html>
+<html dir=""ltr"" lang=""zh-CN"">
+<head>
+<title>{0}</title>
+<meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"" />
+<style type=""text/css"">
+body {font:normal 12px/24px Arial, Helvetica, sans-serif; background:#D9F0DB;}
+textarea,pre {font-family:Courier; font-size:12px;}
+</style>
+</head>
+<body>
+<p><a href='_index.htm'>&lt;&lt;目录</a></p>
+{1}
+<p><a href='_index.htm'>&lt;&lt;目录</a></p>
+</body>
+</html>";
+
+        private Regex reg_con = new Regex(@"<div id=""cnblogs_post_body"">([\s\S]+)</div><div id=""MySignature"">", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
         public async Task Export(List<Article> urls)
         {
             await Export(urls, null);
@@ -42,6 +63,12 @@ namespace Blog.Process
 
             web.URL = article.URL;
             string content = await Task.Run(() => web.Get()).ConfigureAwait(false);
+            Match mat = reg_con.Match(content);
+            if (mat.Success)
+            {
+                content = mat.Groups[1].Value.Trim();
+                return htmlString.Replace("{0}", article.Title).Replace("\n{1}", content); ;
+            }
             return content;
         }
 
@@ -65,7 +92,9 @@ namespace Blog.Process
                 Directory.CreateDirectory(folder);
             }
             var filePath = Path.Combine(folder, article.Title.ToValidFileName() + ".htm");
-            using (var file = new StreamWriter(filePath, false))
+
+            //TODO Change to dynamic get the encoding for page.
+            using (var file = new StreamWriter(filePath, false, Encoding.GetEncoding("UTF-8")))
             {
                 await file.WriteAsync(content);
                 file.Close();
